e3b7d83396fa0a17e3e7fa465bac68d37ec6c4f7
helloimowen <helloimowens@gmail.com>
2018-05-15 17:49:15 -0400

    Added starting files for static page. May add more pages.

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, make_response
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+
+    if request.cookies.get('fun') is '1':
+        return render_template("fun.html")
+
+    else:
+        return render_template("index.html")
+
+
+@app.route("/fun", methods=['POST', 'GET'])
+def fun():
+
+    print(request.cookies.get('fun'))
+
+    if request.cookies.get('fun') is '1':
+        response = make_response(render_template('index.html'))
+        response.set_cookie('fun', '0')
+        return response
+
+    else:
+        response = make_response(render_template('fun.html'))
+        response.set_cookie('fun', '1')
+        return response
+
+    return
